11bf09038761cbe3faec3b11e8c08c099db659c4
Jacob Cabot <166416094+JacobBCabot@users.noreply.github.com>
2024-07-11 15:02:59 +0000

    1st run

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,20 +1,15 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			favorite:[]
 		},
 		actions: {
+			addFavorite: (item) => {
+				setStore({favorite:[...getStore().favorite,item]})
+			},
+			deleteFavorite: (item) => {
+				setStore({favorite:getStore().favorite.filter((x)=> {return x!=item})})
+			},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
---
+++ src/js/views/welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link, useParams} from "react-router-dom";
+import {Contacts} from "../store/appContext";
+
+export const welcome = props => {
+    const {store, actions} = useContext(Context);
+    const params = useParams();
+    return(
+        
+    )
+}
